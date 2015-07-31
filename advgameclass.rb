fd14e10fafe8d264d2b7781121bf1e0319d989fb
idriyslowe <idriyswins@gmail.com>
2015-07-30 20:18:12 -0500

    Major changes to add functionality.

--- advgameclass.rb
+++ advgameclass.rb
@@ -1,33 +1,23 @@
 require './characterclass.rb'
 
 class AdventureGame
-  attr_accessor :total_moves, :direction, :hero
+  attr_accessor :total_moves, :direction, :hero, :enemy
   
   def initialize
     @direction = ["north", "south", "east", "west"]
-    @total_moves = 10
-    puts "Welcome to Idriys's adventure game. Feel free to roam. Beware of enemies and hazards that can hurt you but don't worry. We will help you out along the way. You will have 10 moves total to pass this game."
+    @total_moves = 2
+    puts "You are a guest in Idriys' nightmares. Feel free to roam. Beware of monsters and hazards. They may hurt you but don't worry. Mom will help you out along the way. You will begin with 10 total moves."
+    puts " "
     @hero = Hero.new
   end
-  
-  #mebbe create an @array of enemies so we can call the list in order
-  def rand_enemy
-    newe = Enemy.new
-    fight_or_flight
+
+  def hero_status
+    hero.status
   end
-  
-  def clear_forgive
-    puts "You have encountered the clearance of forgiveness and will pass untouched. Your health is still #{hero.health}"
-  end
-  
-  def rand_haz
-    haz = ["Alligator Pit", "Hell Hounds", "Razorblade Shower"].sample
-    hero.rand_health_loss
-  end
-  
-  #this automatically runs methods in array even though i only intend to call one.
-  def haz_enemy_shuff
-    haz = [rand_haz, rand_enemy, clear_forgive].shuffle[-1]
+
+  def battle_stats
+    puts "#{enemy.name}"
+    # hero.status + enemy.status
   end
   
   def choose_path
@@ -38,42 +28,120 @@ class AdventureGame
       puts "You chose to go #{userinput.upcase}. You will find some suprises along this path."
       2.times do
         haz_enemy_shuff
-        @total_moves -= 1
       end
+      @total_moves -= 1
+      choose_again
     
     else
-      puts "Sorry, I don't understand your choice. Please choose a direction: North, South, East, or West."
+      puts "Sorry, I don't understand your choice."
+      choose_path
     end
   end
   
-  def fight_or_flight
-    puts 'To fight or not to fight? You may gain strength from winning this battle but losing could lose your life. Running will cause you to lose 2 health and 2 strength. Press "1" to FIGHT or "2" to RUN.'
+  def choose_again
+    puts 'Do you wish to continue down this path or change direction? Type "1" to Continue and "2" to Change Direction.'
     userinput = gets.chomp
 
-    loop do unless userinput == "1" || userinput == "2"
-      if userinput == "1"
-        hero.rand_health_loss
-      end
+    if userinput == "1"
+      puts "You chose to CONTINUE. Let's see what awaits us down this path."
+      @total_moves -= 1
+      haz_enemy_shuff
 
-      if userinput == "2"
-        hero.run_health_loss
-        return
-      end
-
-      else
-        puts 'Fight or flight!? Press "1" to FIGHT or "2" to RUN.'
-      end
+    elsif userinput == "2"
+      puts "You chose to CHANGE DIRECTION. Let's see what awaits us down this path."
+      choose_path
+      @total_moves -= 1
+    
+    else
+      puts "Sorry, I don't understand your choice."
+      choose_again
     end
   end
-  
-  #find a place in script to throw weapon in
+
+  def fight_or_flight
+    puts 'To fight or not to fight? You may gain strength from winning this battle but losing could lose you your life. Running will cause you to lose 2 health and 2 strength. Press "1" to FIGHT, "2" to RUN, or "status" to see yours and the enemy\'s status.'
+    userinput = gets.chomp
+
+      if userinput == "status"
+        battle_stats
+      end
+
+      if userinput == "1"
+        puts "You chose to FIGHT! Do you wish to use a weapon?"
+
+        if hero.strength > enemy.strength
+        hero.strength += 15
+        puts "You win the battle against #{enemy.name} and gain 15 strength."
+        hero.rand_health_loss
+
+        elsif hero.strength < enemy.strength
+           hero.strength -= 15
+           puts "#{enemy.name} wins the battle."
+           hero.rand_health_loss
+
+        else
+          hero.strength -= 7 and enemy.strength -= 7
+          puts "It's a draw! You lose 7 strength."
+          hero.rand_health_loss
+        end
+
+      elsif userinput == "2"
+        puts "You chose to run."
+        hero.run_health_loss
+
+      else
+        fight_or_flight
+      end
+  end
+
+#next code was the only way not to invoke all of the methods. arrays and hashes failed at this.
+  def haz_enemy_shuff
+    choice = rand(3)
+
+    case
+    when choice == 0
+      rand_enemy
+
+    when choice == 1
+      clear_forgive
+
+    when choice == 2
+      rand_haz
+
+    when choice == 3
+      found_weapon
+    end
+  end
+
+  # def enemy
+  #   @enemy = Enemy.new
+  # end
+
+  def rand_enemy
+    @enemy = Enemy.new
+    fight_or_flight
+  end
+
+  def clear_forgive
+    puts "You have encountered the clearance of forgiveness and will pass untouched. Your health is still #{hero.health}. \n\r"
+  end
+
+  def rand_haz
+    haz = ["Alligator Pit", "Hell Hounds", "Razorblade Shower"].sample
+    puts "You have encountered the #{haz}."
+    hero.rand_health_loss
+  end
+
   def found_weapon
-    names = ["Ball-shredder", "Dorkinator", "Numbnutter", "Tittilator", "Tickler"].shuffle[-1]
+    name = ["Ball-shredder", "Dorkinator", "Numbnutter", "Tittilator", "Tickler"].shuffle[-1]
     damage = rand(6)
-    puts "You found a weapon called #{names} worth #{damage} damage!"
+    hero.weapon = name
+    puts "You found a weapon called #{hero.weapon} worth #{damage} damage! \n\r"
     hero.strength += damage
   end
 end
 
 idriys = AdventureGame.new
-idriys.choose_path
+idriys.battle_stats
+
+
--- mygame.rb
+++ mygame.rb
@@ -1,31 +1,18 @@
 require './characterclass.rb'
 require './advgameclass.rb'
 
+#this loops forever!!!
 idriys = AdventureGame.new
-while idriys.total_moves > 0
+while idriys.total_moves >= 0
   
   idriys.choose_path
-  
 
 end
 
-#need to create method that creates enemy to fight to referece in below method. also create weapon module that will add strength. #what can enemy do that hero can't do? Die.
-# def fight(enemy)
-#     if hero.strength > enemy.strength
-#         hero.strength += 5
-#         enemy.strength -= 5
-#         puts "#{hero.name} wins the battle and gains 5 strength. #{enemy.name} loses 5 strength."
-#     elsif hero.strength > enemy.strength
-#        hero.strength += 5
-#        enemy.strength -= 5
-#        puts "#{enemy.name} wins the battle and gains 5 strength. #{hero.name} loses 5 strength."
-#     else
-#       hero.strength -= 2.5 and enemy.strength -= 2.5
-#       puts "It's a draw! You both lost 2.5 strength."
-#     end
-#   end
+puts "Thank you for playing but you've run out of moves!"
 
-#mebbe create a file to write to that keeps enemies and hero info so we don't have to worry about this code keeping that
 #formatador ruby gem
+
+#i need to come up with some skill here. right now we're just choosing direction and if whether to fight. need to use weapons. pokemon?
 
 
